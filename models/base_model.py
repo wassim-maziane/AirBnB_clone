12b73664dc0b64cd0cbfd1d9896d45396f665d12
MAZIANE wassim <mazianewassim@gmail.com>
2023-10-31 11:45:19 +0100

    fixed test_base_model codestyle

--- models/base_model.py
+++ models/base_model.py
@@ -2,31 +2,35 @@
 import uuid
 from models import storage
 from datetime import datetime
+
+
 class BaseModel:
     def __init__(self, **kwargs):
-        if len(kwargs) == 0:    
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)	
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
                 elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime(value, timeformat))
                 else:
                     setattr(self, key, value)
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-       	storage.save() 
-       	self.updated_at = datetime.now()
+        storage.save()
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         dic = self.__dict__.copy()
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
         dic['updated_at'] = self.updated_at.isoformat()
         return dic
-
-
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -11,5 +11,6 @@ print(my_model)
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
+typ = type(my_model_json[key])
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    print("\t{}: ({}) - {}".format(key, typ, my_model_json[key]))
